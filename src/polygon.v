f29f39bf6bc85dd360a82576e3262038b98df2f8
Fibletype <egor.rusakov@technavology.com>
2021-12-16 19:18:08 +0300

    add comments

--- src/polygon.v
+++ src/polygon.v
@@ -21,7 +21,7 @@
 то есть выглядеть это должно примерно так:
 F (G ((a -> b) or (a -> c)))
 
-Давайте пока начнем с просто G.
+Кажется, что формулу над состоянием можно просто заменить на набор условий, что здесь сейчас и описано 
 
 *)
 
@@ -29,19 +29,25 @@ Require Import List.
 Require Import Lia.
 Import ListNotations.
 
+(* Условия по которым мы разбиваем систему контрактов на состояния *)
+
 Inductive cond :=
 | phi
 | psi
 | ksi.
 
+(* Состояние это набор условий *)
 
 Definition state := list cond.
+
+(* Несколько состояний *)
 
 Definition A := [phi ; psi].
 Definition B := [psi ; phi].
 Definition C := [ksi ; phi].
 Definition D := [psi ; ksi].
 
+(* Переходы между состояниями, которые происходят при обработке сообщений *)
 
 Inductive rel : state -> state -> Prop := 
 | AC : rel A C
@@ -51,34 +57,58 @@ Inductive rel : state -> state -> Prop :=
 
 (* G phi /\ F psi /\ phi U psi *)
 
+(* Предикат, который гарантирует, что сценарий корректен с точки зрения переходов между состояниями *)
+
 Inductive rel_prop : list state -> Prop :=
 | empty_rel : rel_prop []
 | base_rel : forall a, rel_prop [a]
 | trans_rel : forall a b l , rel a b -> rel_prop (b :: l) -> rel_prop (a :: b :: l).
 
-Definition state_formula := state -> Prop.
+(* Definition state_formula := state -> Prop. *)
 
 (* 
 Inductive state_formula_impl : cond -> state -> Prop :=
 | impl : forall c1 c2 s, In c1 s -> In c2 s -> state_formula_impl c1 s.
  *)
+
+
+(* Предикаты, которые позволяют нам судить о выполнении каких-то свойств в сценарии *)
+
 Inductive G_formula : list cond -> list state -> Prop :=
 | G_empty_form : forall (b : list cond), G_formula b []
 | G_hold : forall (a : state) (b : list cond) (c : list state), Forall (fun x => In x a) b -> G_formula b c -> G_formula b (a::c).
 
-Inductive F_formula : cond -> list state -> Prop :=
-| F_empty_form : forall (b : cond), F_formula b []
-| F_not_hold : forall (a : state) (b: cond) (s : list state), not (In b a) -> F_formula b s -> F_formula b (a::s)
-| F_hold_first_time : forall (a : state) (b : cond) (s : list state), In b a -> F_formula b s -> F_formula b (a::s).
+(* Некорректный предикат F. Если мы всегда будем использовать второй конструктор, то можем показать выполнимость этого предиката на сценарии, 
+    который не удовлетворяет нужным свойствам. Поэтому для этого предиката в доказательствах обязательно должен встретиться 3 конструктор. *)
 
+Inductive F_formula : list cond -> list state -> Prop :=
+| F_empty_form : forall (b : list cond), F_formula b []
+| F_not_hold : forall (a : state) (b: list cond) (s : list state), not (Forall (fun x => In x a) b) -> F_formula b s -> F_formula b (a::s)
+| F_hold_first_time : forall (a : state) (b : list cond) (s : list state), Forall (fun x => In x a) b -> F_formula b s -> F_formula b (a::s).
+
+(* Его можно исправить на предикат, который будет говорить, что наше условие выполняется всегда в последний момент, однако это не будет 
+    соответствовать общепринятому значению оператора F *)
+
+(* Inductive FG_formula : list cond -> list state -> Prop :=
+| FG_empty_form : forall (b : list cond), FG_formula b []
+| FG_not_hold : forall (a : state) (b: list cond) (s : list state), not (Forall (fun x => In x a) b) -> FG_formula b s -> FG_formula b (a::s)
+| FG_hold_first_time : forall (a : state) (b : list cond) (s : list state), Forall (fun x => In x a) b -> FG_formula b s -> FG_formula b (a::s).
+  *)
+
+
+(* Определение сценариев *)
 
 Definition scen1 := [ A ; B ; C ].
 Definition scen2 := [ A ; D ].
+
+(* Корректность сценария *)
 
 Lemma scen1_correct : rel_prop scen1.
 Proof.
     repeat constructor.
 Qed.
+
+(* Выполнимость формулы G phi на первом сценарии *)
 
 Lemma scen1_G_phi : G_formula [phi] scen1.
 Proof.
@@ -88,6 +118,7 @@ Proof.
     constructor. constructor. 
 Qed.
 
+(* формула G phi/\psi не выполняется на первом сценарии, хотя непонятно как это реально объяснять*)
 
 Lemma scen1_G_phi_psi : G_formula [phi ; psi] scen1.
 Proof.
@@ -103,25 +134,23 @@ Proof.
     constructor.
     simpl.
 Admitted.
+    
+(* Доказательство выполнимости формулы F ksi на первом сценарии *)   
 
-Lemma scen2_G_phi : G_formula [phi] scen2.
-Proof.
-    constructor. constructor.
-    simpl. left. auto.
-    constructor. constructor. constructor.
-    simpl.
-Admitted.
-    
-    
-Lemma scen1_F_ksi : F_formula ksi scen1.
+Lemma scen1_F_ksi : F_formula [ksi] scen1.
 Proof.
     constructor.
     simpl. unfold not. intros.
-    inversion H. discriminate.
-    inversion H0. discriminate. assumption.
+    inversion H. inversion H2. discriminate.
+    inversion H4. discriminate. assumption.
     constructor. simpl. unfold not. intros.
-    inversion H. discriminate.
-    inversion H0. discriminate. assumption.
-    constructor 3. simpl. left. auto.
-    constructor.
+    inversion H. inversion H2. discriminate.
+    inversion H4. discriminate. assumption.
+
+    (* ***************** *)
+    constructor 3.
+    (* Без этой строчки получается бесполезное доказательство, если я всегда пользовался только первым и вторым конструктором *)
+
+    simpl. constructor. left. auto.
+    constructor. constructor.
 Qed.
